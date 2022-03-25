54884185ca129c48baf20077d5e0240996e78f9a
Jesus Aranda <jesusmaria.aranda@mercadolibre.com>
2022-03-25 10:58:55 -0300

    Creating the Input styled component

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,50 @@
-import React from "react";
-import { Layout } from "components/common";
+import React, { useState } from "react";
+import { Layout, Input } from "components/common";
+import styled from "styled-components";
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 400px;
+  background: white;
+  border: 11px solid #eee;
+  padding: 10px;
+  box-sizing: border-box;
+  color: black;
+  border-radius: 4px;
+`;
 
 const Login = () => {
-  return <Layout>Login</Layout>;
+  const [formFields, setFormFields] = useState({ username: "", password: "" });
+
+  const handleInputChange = (e) => {
+    e.persist();
+    setFormFields((prevValue) => ({
+      ...prevValue,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  return (
+    <Layout>
+      <h1>Login</h1>
+      <Form>
+        <Input
+          name="username"
+          placeholder="username"
+          value={formFields.username}
+          type="text"
+          onChange={handleInputChange}
+        />
+
+        <Input
+          name="password"
+          placeholder="password"
+          value={formFields.password}
+          type="password"
+          onChange={handleInputChange}
+        />
+      </Form>
+    </Layout>
+  );
 };
 
 export default Login;
